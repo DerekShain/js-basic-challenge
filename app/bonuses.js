44e1482b9f7ffc8f010dc89d4161a2da625fb54c
Derek Shain <derek0anthony@gmail.com>
2021-08-23 21:03:46 -0600

    bonus

--- app/bonuses.js
+++ app/bonuses.js
@@ -2,16 +2,26 @@
 let str = "team number 2"
 
 function camelCase(str) {
-  let result="";
-  for(let i=0, len = str.length; i<len; i++){
-      let currentStr = str[i];
-      let tempStr = currentStr.toLowerCase();
-      if(i !=0){
-          tempStr=tempStr.substr(0,1).toUpperCase()+tempStr.substr(1);
-      }
-      result +=tempStr;
-  }
-  console.log(result)
-  return result;
+
+    let newString = ""
+    newString = str
+    let s = "";
+ 
+    for (let i = 0; i < newString.length; i++) {
+
+        // check for spaces in the sentence
+        if (newString[i] == ' ') {
+            // conversion into upper case
+            s += newString[i+1].toUpperCase();
+            i++;
+        }
+ 
+        // If not space, copy character
+        else
+        s += newString[i];        
+    }
+ 
+    // return string to main
+    return s;
 }
 
